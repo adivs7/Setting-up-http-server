30adf3cb02694b287c7509c2c66066d43a2bd688
adiv7 <adityavs167@gmail.com>
2024-02-29 15:24:14 +0530

    made an crud api to create,read,update and delete blogs

--- Using Express/index1.js
+++ Using Express/index1.js
@@ -1,24 +1,47 @@
+const bodyParser = require('body-parser');
 const express = require('express');
 // Create an express app object using express function
 const app=express();
 const PORT=3000;
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:true})); 
+let bloglist=[];
 
-app.get('/', function (request,response){
-    response.send("welcome to my first express server");
+app.get('/blogs', function (request,response){
+    return response.status(404).json({
+        data:bloglist,
+        status:404,
+        success:true
+    })
 })
-app.get('/home', function(req,res){
-   // console.log(req);
-   res.json({
-      message :"this is a get request in home",
-      success: true
+
+// app.get('/home', function(req,res){
+//    // console.log(req);
+//    res.json({
+//       message :"this is a get request in home",
+//       success: true
+//    })
+//     res.send("welcome to home my dear");
+// })
+
+app.post('/blogs',function(req,res){
+   bloglist.push({
+    title: req.body.title, 
+    content: req.body.content,
+    id:Math.random(1000)});
+   return response.status(404).json({
+    status:true
    })
-    res.send("welcome to home my dear");
 })
 
-app.post('/home',function(req,res){
-    res.send("Post request");
+app.get('id/blogs/', (req,res)=>{
+    const result=bloglist.filter( (blog)=> blog.id==req.param.id);
+    return res.status(200).json({
+        data:result,
+        success:true
+    }) 
 })
 
 app.listen(PORT, function exec(){
-    console.log("Server Started");
+    console.log("Server Started in, ", PORT);
 })
